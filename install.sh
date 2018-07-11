c2ecfd934f92ba008be0cf44eea4699cd5233ee3
Kingrimursel <kingrimursel@ploetze.com>
2018-07-11 20:20:31 +0200

    Removing line break from install.sh

--- install.sh
+++ install.sh
@@ -20,4 +20,4 @@ sed -i "40i\destination_folder=$PWD/Backups" "$PWD/BaWaB.sh"
 (crontab -l 2>/dev/null; echo "0 * * * * env USER=$LOGNAME $PWD/BaWaB.sh cron") | crontab -
 
 # Inform the user
-echo -e "\n${GREEN}Installation completed, a cron job was set to be triggered every hour. Now you need to define the folders that should be backed up and some other variables of you want. Either read the README or the first lines of BaWab.sh to get more informations.${NC}"
+echo -e "${GREEN}Installation completed, a cron job was set to be triggered every hour. Now you need to define the folders that should be backed up and some other variables of you want. Either read the README or the first lines of BaWab.sh to get more informations.${NC}"
